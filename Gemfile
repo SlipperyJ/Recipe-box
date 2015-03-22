43119b1f710d63273897c442a945c504f4f6c322
Ross Hamilton <rosshamilton.dev@gmail.com>
2015-03-22 00:09:22 +0000

    Added bootstrap styling and all relevant paths for new recipes

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'haml', '4.0.5'
 gem 'simple_form', '3.0.2'
+gem 'bootstrap-sass', '3.2.0.2'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ class RecipesController < ApplicationController
   before_action :find_recipe, only: [:show, :edit, :update, :destroy]
   
   def index
+    @recipe = Recipe.all.order("created_at desc")
   end
   
   def show
@@ -21,6 +22,22 @@ class RecipesController < ApplicationController
     end      
   end
   
+  def edit
+  end
+  
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @recipe.destroy
+    redirect_to root_path, notice: "Successfully deleted recipe"
+  end
+    
   private
   
   def recipe_params
@@ -28,6 +45,6 @@ class RecipesController < ApplicationController
   end
   
   def find_recipe
-    @recipe = Recipe.find(params [:id])
+    @recipe = Recipe.find(params[:id])
   end
 end
