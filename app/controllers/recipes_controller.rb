08cf34130266f3687f5086805fb9cc43ef86b9c1
Ross Hamilton <rosshamilton.dev@gmail.com>
2015-03-25 23:39:49 +0000

    Added user functions and sign up page styling

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,30 +1,31 @@
 class RecipesController < ApplicationController
   before_action :find_recipe, only: [:show, :edit, :update, :destroy]
-  
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @recipe = Recipe.all.order("created_at DESC")
   end
-  
+
   def show
   end
-  
+
   def new
-    @recipe = Recipe.new
+    @recipe = current_user.recipes.build
   end
-  
+
   def create
-    @recipe = Recipe.new(recipe_params)
-    
+    @recipe = current_user.recipes.build(recipe_params)
+
     if @recipe.save
       redirect_to @recipe, notice: "Successfully created new recipe"
     else
       render 'new'
-    end      
+    end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @recipe.update(recipe_params)
       redirect_to @recipe
@@ -32,19 +33,20 @@ class RecipesController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @recipe.destroy
     redirect_to root_path, notice: "Successfully deleted recipe"
   end
-    
+
   private
-  
+
   def recipe_params
-		params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
+    params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
   end
-  
+
   def find_recipe
     @recipe = Recipe.find(params[:id])
   end
 end
+
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 
 class Recipe < ActiveRecord::Base
+	belongs_to :user
 
 	has_many :ingredients
 	has_many :directions
@@ -11,6 +12,7 @@ class Recipe < ActiveRecord::Base
   															reject_if: proc { |attributes| attributes['step'].blank? },
   															allow_destroy: true
 
+  	validates :title, :description, :image, presence: true
 
 	has_attached_file :image, styles: { :medium => "400x400#" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
